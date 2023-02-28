454cd4263e895f644b558baa2ba21cd684d8074a
Jackson Watt-Bowers <wattbowers@gmail.com>
2023-02-28 17:50:06 -0500

    added sign up functionality

--- script/script.js
+++ script/script.js
@@ -30,24 +30,34 @@ loginFormElement.addEventListener('submit', e => {
         currentUser = user
       }
     })
-    console.log(currentUser)
 })
 
 signupFormElement.addEventListener('submit', e => {
   e.preventDefault()
   const givenUsername = e.target[0].value
   const givenPassword = e.target[1].value
+  let isTakenUsername = false; 
+  const userObject = ref(database, 'users');
 
-  const userObject = ref(database, 'users')
+  // check to see if username already exists
 
-  push(userObject, constructUser(givenUsername, givenPassword))
+  for(let user in frontEndData.users) {
+    let checkUser = frontEndData.users[user].username
+    if(givenUsername === checkUser) {
+      isTakenUsername = true;
+      break;
+    }
+  }
+  //Checking to see if username is already taken, if not will create new user and push to database
+  if (isTakenUsername === false) {
+    push(userObject, constructUser(givenUsername, givenPassword));
+    currentUser = constructUser(givenUsername, givenPassword);
+  }
 })
 
 onValue(dbRef, (data) => {
     if(data.exists()){
       frontEndData = data.val()
-    }else{
-      console.log("No data available")
     }
   })
 
