887bc1b601df6909ddf19e70d441b35b091e2f9f
Galen White <galenhwhite419@gmail.com>
2023-03-02 18:02:25 -0500

    Setting up addMovie functionality

--- script/functionality.js
+++ script/functionality.js
@@ -1,7 +1,20 @@
+import firebase from './firebase.js'
+import { getDatabase, ref, onValue, push } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js";
+
+const database = getDatabase(firebase);
+const dbRef = ref(database);
+
+let frontEndData;
 const logOutButton = document.getElementById('logOut');
 const currentUser = JSON.parse(window.localStorage.getItem('user'));
 
 logOutButton.addEventListener('click', e => {
   window.localStorage.removeItem('user');
   window.location.assign('index.html');
-  })  
+  })
+
+  onValue(dbRef, (data) => {
+    if (data.exists()){
+      frontEndData = data.val();
+    }
+  })
