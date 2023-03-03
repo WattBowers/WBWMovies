19f207fd9b01a469fe3d34d54451575b4b8ae046
Galen White <galenhwhite419@gmail.com>
2023-03-03 12:00:09 -0500

    Setting up JS list functionality

--- script/functionality.js
+++ script/functionality.js
@@ -11,10 +11,41 @@ const currentUser = JSON.parse(window.localStorage.getItem('user'));
 logOutButton.addEventListener('click', e => {
   window.localStorage.removeItem('user');
   window.location.assign('index.html');
-  })
+})
 
-  onValue(dbRef, (data) => {
-    if (data.exists()){
-      frontEndData = data.val();
+const moviesRef = ref(database, `/${currentUser.movies}`);
+console.log(currentUser);
+const moviesUl = document.querySelector('.moviesList');
+
+onValue(dbRef, (data) => {
+  if (data.exists()) {
+    moviesUl.innerHTML = '';
+    const movieList = data.val();
+    for (let key in movieList) {
+      const title = movieList[key];
+      const runtime = movieList[key].runtime;
+      const genre = movieList[key].genre;
+      const year = movieList[key].year;
+      const synopsis = movieList[key].synopsis;
+
+      const li = document.createElement('li');
+      const h3 = document.createElement('h3');
+
+      h3.textContent = title;
+
+      moviesUl.append(li);
+      li.append(h3);
     }
-  })
+  }
+})
+
+const movieForm = document.querySelector('#movieForm');
+
+movieForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const title = document.getElementById('movieTitle');
+  const runtime = document.getElementById('runtime');
+  const genre = document.getElementById('movieGenre');
+  const year = document.getElementById('movieYear');
+  const synopsis = document.getElementById('movieSynopsis');
+})
