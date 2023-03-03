fe017a003882170db9459897b0ccd4fe796e62ca
Jackson Watt-Bowers <wattbowers@gmail.com>
2023-03-03 17:29:36 -0500

    Connected movie database

--- script/functionality.js
+++ script/functionality.js
@@ -1,14 +1,17 @@
 import firebase from './firebase.js'
-import { getDatabase, ref, onValue, push } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js";
+import { getDatabase, ref, onValue, push, get } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js";
 
 const database = getDatabase(firebase);
 const dbRef = ref(database);
 
-let frontEndData;
+let frontEndData = {};
 const logOutButton = document.getElementById('logOut');
-const currentUser = JSON.parse(window.localStorage.getItem('user'));
+const testButton = document.getElementById('test');
+let currentUser = JSON.parse(window.localStorage.getItem('user'));
 
-
+testButton.addEventListener('click', e => {
+  console.log(frontEndData)
+})
 
 logOutButton.addEventListener('click', e => {
   window.localStorage.removeItem('user');
@@ -19,33 +22,50 @@ logOutButton.addEventListener('click', e => {
 console.log(currentUser);
 const moviesUl = document.querySelector('.moviesList');
 
-onValue(dbRef, (data) => {
-  frontEndData = data.val();
-  for(let user in frontEndData.users) {
-    if (currentUser.username === user.username) {
-      console.log(currentUser, user)
-      currentUser = user;
+const connectFrontEnd = (data) => {
+  for(let user in data.users) {
+    if (currentUser.username === data.users[user].username) {
+      currentUser = data.users[user];
+      const userRef = ref(database, 'users/' + user + '/movies');
+      onValue(userRef, (data) => {
+        console.log(data.val())
+      })
     }
   }
-  
+}
+
+onValue(dbRef, (data) => {
+  console.log('were here')
   if (data.exists()) {
+    connectFrontEnd(data.val());
+    
+    //connecting database to currentUser
+    
+  
+  
     moviesUl.innerHTML = '';
     const movieList = data.val();
-    for (let key in movieList) {
-      const title = movieList[key];
-      const runtime = movieList[key].runtime;
-      const genre = movieList[key].genre;
-      const year = movieList[key].year;
-      const synopsis = movieList[key].synopsis;
+    console.log(currentUser)
+  for (let key in currentUser.movies) {
+    console.log(key)
+  }
 
-      const li = document.createElement('li');
-      const h3 = document.createElement('h3');
-
-      h3.textContent = title;
-
-      moviesUl.append(li);
-      li.append(h3);
-    }
+    //for (let key in movieList.users) {
+    //  const title = movieList[key];
+    //  console.log(movieList.users[key].movies)
+    //  const runtime = movieList[key].runtime;
+    //  const genre = movieList[key].genre;
+    //  const year = movieList[key].year;
+    //  const synopsis = movieList[key].synopsis;
+//
+    //  const li = document.createElement('li');
+    //  const h3 = document.createElement('h3');
+//
+    //  h3.textContent = title;
+//
+    //  moviesUl.append(li);
+    //  li.append(h3);
+    //}
   }
 })
 
